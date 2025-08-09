07e3ce08dfeebe329410fa0dc0a0e46f5153e557
shivamkumar <shivamkumareng7@gmail.com>
2025-08-09 23:41:40 +0530

    database connected

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const { AuthRouter } = require("./routes/auth");
+const app = express();
+
+
+app.use(express.json());
+
+app.use("/user/auth", AuthRouter);
+
+// DB connect
+async function DbConnect(){
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("done")
+}
+DbConnect();
+
+app.listen(3000);
---
+++ server/routes/auth.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const AuthRouter = Router();
+
+
+module.exports = {
+    AuthRouter: AuthRouter
+}
