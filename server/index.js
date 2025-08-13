d2acc01e8e0a629a966062d9cf0748b95efd5fcb
shivamkumar <shivamkumareng7@gmail.com>
2025-08-13 22:40:48 +0530

    implemented google oauth login

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
 const { AuthRouter } = require("./routes/auth");
+const session = require("express-session");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+
 const app = express();
-
-
 app.use(express.json());
+
+// auth setup
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 60*60*1000
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/schedmate/auth", AuthRouter);
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,61 @@
 const { Router } = require("express");
 const AuthRouter = Router();
+require("dotenv").config();
+const session = require("express-session");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const { UserModel } = require("../db");
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/schedmate/auth/verify"
+    },
+    function(accessToken, refreshToken, profile, done){
+        const user = {
+            accessToken,
+            refreshToken,
+            profile
+        }
+
+        return done(null, user)
+    }
+));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+//FE: CLICK ON SINGIN IN FE AND YOU'LL BE HERE
+AuthRouter.get("/signin", passport.authenticate('google', {
+    scope: [
+        'profile',
+        'email',
+        'https://www.googleapis.com/auth/calendar'
+    ],
+    prompt: 'consent login'
+}));
+
+
+// after authentication we'll be redirected to this page
+AuthRouter.get("/verify", passport.authenticate('google', {failureRedirect: "/"}),
+
+    async function(req, res){                                                                // here this 'req' have access to the profile and email and for calendar you have to call through the api.
+        const email = req.user.profile.email;
+        const id = req.user.profile.id;
+
+        const user = await UserModel.findOne({
+            email,
+            id
+        });
+
+        if(user == undefined){
+            res.redirect("/setup")                                  // if no profile has been setup already by the user
+        }
+        else{
+            res.redirect("/main")                                   // if there exists a profile of the user
+        }
+    }
+);
 
 module.exports = {
     AuthRouter: AuthRouter
